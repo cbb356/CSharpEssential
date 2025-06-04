6074bd7b2400eee1eb3cf38636d1dddcd18c7825
cbb356 <cbb356@gmail.com>
2025-06-05 02:10:21 +0300

    001_Classes_02_Rectangle added

---
+++ 001_Classes/001_Classes_HW/02_Rectangle/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first side of the rectangle (positive double): ");
+            if (!double.TryParse(Console.ReadLine(), out double side1) || side1 <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive double.");
+                return;
+            }
+
+            Console.Write("Enter the second side of the rectangle (positive double): ");
+            if (!double.TryParse(Console.ReadLine(), out double side2) || side2 <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive double.");
+                return;
+            }
+
+            Rectangle rectangle = new Rectangle (side1, side2);
+
+            Console.WriteLine($"The perimeter of the rectangle with sides {side1} and {side2} " +
+                $"is: {rectangle.Perimeter}");
+            Console.WriteLine($"The area of the rectangle with sides {side1} and {side2} " +
+                $"is: {rectangle.Area}");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
