da89b6dc497a86b692f3f2f711a8b4222ca6280b
cbb356 <cbb356@gmail.com>
2025-06-06 21:44:26 +0300

    001_02_HW edited

--- 001_Classes/001_Classes_HW/03_Books/Book.cs
+++ 001_Classes/001_Classes_HW/03_Books/Book.cs
@@ -8,29 +8,53 @@ namespace Classes
 {
     internal class Book
     {
-        public Author Author { get; set; }
-        public Title Title { get; set; }
-        public Content Content { get; set; }
+        private Author _author;
+        private Title _title;
+        private Content _content;
+        
+        public string Author 
+        { 
+            set 
+            { 
+                _author.Value = value;
+            } 
+        }
+
+        public string Title
+        {
+            set
+            {
+                _title.Value = value;
+            }
+        }
+
+        public string Content
+        {
+            set
+            {
+                _content.Value = value;
+            }
+        }
 
         public Book() 
         {
-            Author = new Author();
-            Title = new Title();
-            Content = new Content();
+            _author = new Author();
+            _title = new Title();
+            _content = new Content();
         }
 
         public Book(string author, string title, string content) : this()
         {
-            Author.Value = author;
-            Title.Value = title;
-            Content.Value = content;
+            _author.Value = author;
+            _title.Value = title;
+            _content.Value = content;
         }
 
         public void Show()
         {
-            Author.Show();
-            Title.Show();
-            Content.Show();
+            _author.Show();
+            _title.Show();
+            _content.Show();
         }
     }
 }
--- 001_Classes/001_Classes_HW/03_Books/Program.cs
+++ 001_Classes/001_Classes_HW/03_Books/Program.cs
@@ -6,13 +6,16 @@
         {
             Book book1 = new Book();
             book1.Show();
+            Console.WriteLine();
 
+            book1.Author = "Adam Smith";
+            book1.Title = "The Theory of Moral Sentiments ";
+            book1.Content = "Economical Studies";
+            book1.Show();
             Console.WriteLine();
 
             Book book2 = new Book("John Doe", "The Book", "Some text");
             book2.Show();
-
-            Console.WriteLine();
 
             // Delay.
             Console.ReadKey();
