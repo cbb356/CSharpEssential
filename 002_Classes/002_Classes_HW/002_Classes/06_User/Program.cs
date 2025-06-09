5ecf290d194356863afdc181cb67ea02d592f131
cbb356 <cbb356@gmail.com>
2025-06-09 23:44:06 +0300

    002_04_HW project solution addded

---
+++ 002_Classes/002_Classes_HW/002_Classes/06_User/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("user", "John", "Doe", 25);
+            user.ShowUserInfo();
+        }
+    }
+}
---
+++ 002_Classes/002_Classes_HW/002_Classes/06_User/User.cs
@@ -0,0 +1,25 @@
+﻿namespace Classes
+{
+    internal class User
+    {
+        private string login;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private readonly DateTime registeredAt;
+
+        public User (string login, string firstName, string lastName, int age)
+        {
+            this.login = login;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.registeredAt = DateTime.Now;
+        }
+
+        public void ShowUserInfo()
+        {
+            Console.WriteLine($"User info with login {login}: {firstName} {lastName}, {age} years old, registered at {registeredAt}");
+        }
+    }
+}
