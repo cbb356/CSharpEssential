d303aede562b82688cd22063f5bf735fe2b1e6c6
cbb356 <cbb356@gmail.com>
2025-06-10 00:28:53 +0300

    002_02_HW added

---
+++ 002_Classes/002_Classes_HW/002_Classes/02_Converter/Converter.cs
@@ -0,0 +1,44 @@
+﻿namespace Classes
+{
+    internal class Converter
+    {
+        private double usd;
+        private double eur;
+        private double gbp;
+        public Converter(double usd, double eur, double gbp)
+        {
+            this.usd = usd;
+            this.eur = eur; 
+            this.gbp = gbp;
+        }
+
+        public double ConvertUsdUah(double sum)
+        { 
+            return sum * usd;
+        }
+
+        public double ConvertEurUah(double sum)
+        {
+            return sum * eur;
+        }
+
+        public double ConvertGbpUah(double sum)
+        {
+            return sum * gbp;
+        }
+        public double ConvertUahUsd(double sum)
+        {
+            return sum / usd;
+        }
+
+        public double ConvertUahEur(double sum)
+        {
+            return sum / eur;
+        }
+
+        public double ConvertUahGbp(double sum)
+        {
+            return sum / gbp;
+        }
+    }
+}
---
+++ 002_Classes/002_Classes_HW/002_Classes/02_Converter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(41.5, 47.1, 55.9);
+            double cur = 100, uah = 1000;
+            Console.WriteLine($"{cur} USD = {converter.ConvertUsdUah(cur):F2} UAH");
+            Console.WriteLine($"{cur} EUR = {converter.ConvertEurUah(cur):F2} UAH");
+            Console.WriteLine($"{cur} GBP = {converter.ConvertGbpUah(cur):F2} UAH");
+            Console.WriteLine($"{uah} UAH = {converter.ConvertUahUsd(uah):F2} USD");
+            Console.WriteLine($"{uah} UAH = {converter.ConvertUahEur(uah):F2} EUR");
+            Console.WriteLine($"{uah} UAH = {converter.ConvertUahGbp(uah):F2} GBP");
+        }
+    }
+}
