37e7cd429ac9374ba7d67d2dfc03da2822794fac
cbb356 <cbb356@gmail.com>
2025-06-10 22:23:04 +0300

    002_04_HW added

---
+++ 002_Classes/002_Classes_HW/04_Invoice/Invoice.cs
@@ -0,0 +1,27 @@
+﻿namespace Classes
+{
+    internal class Invoice
+    {
+        private readonly int account;
+        private readonly string customer;
+        private readonly string provider;
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        public double PricePerItem { get; set; }
+        public Invoice(int account, string customer, string provider) 
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+
+        public void TotalCost()
+        {
+            double totalCost = Quantity * PricePerItem;
+            double totalCostVAT = totalCost * 1.2;
+            Console.WriteLine($"Invoice {account}");
+            Console.WriteLine($"Customer: {customer}, Provider: {provider}");
+            Console.WriteLine($"The total cost for {Quantity} of {Article} is: {totalCost:F2} ({totalCostVAT:F2} including VAT)");
+        }
+    }
+}
--- 002_Classes/002_Classes_HW/04_Invoice/Program.cs
+++ 002_Classes/002_Classes_HW/04_Invoice/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Invoice invoice = new Invoice(15, "Customer Company", "Provider Company")
+            {
+                Article = "goods",
+                Quantity = 18,
+                PricePerItem = 113.45
+            };
+
+            invoice.TotalCost();
         }
     }
 }
