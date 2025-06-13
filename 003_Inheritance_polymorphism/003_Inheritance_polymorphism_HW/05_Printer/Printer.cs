da0ac9f3dbaf6a79f03d6b1edd3c68284e248dd2
cbb356 <cbb356@gmail.com>
2025-06-13 22:50:58 +0300

    003_05_HW added

---
+++ 003_Inheritance_polymorphism/003_Inheritance_polymorphism_HW/05_Printer/Printer.cs
@@ -0,0 +1,40 @@
+﻿namespace Inheritance_polymorphism
+{
+    internal class Printer
+    {
+        public virtual void Print(string value)
+        { 
+            Console.WriteLine(value);
+        }
+    }
+
+    internal class BluePrinter : Printer 
+    {
+        public override void Print(string value)
+        { 
+            Console.ForegroundColor = ConsoleColor.Blue;
+            base.Print(value);
+            Console.ResetColor();
+        }
+    }
+
+    internal class GreenPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            base.Print(value);
+            Console.ResetColor();
+        }
+    }
+
+    internal class YellowPrinter : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            base.Print(value);
+            Console.ResetColor();
+        }
+    }
+}
--- 003_Inheritance_polymorphism/003_Inheritance_polymorphism_HW/05_Printer/Program.cs
+++ 003_Inheritance_polymorphism/003_Inheritance_polymorphism_HW/05_Printer/Program.cs
@@ -10,7 +10,26 @@
          */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Printer basePrinter = new Printer();
+            basePrinter.Print("Printer as Printer");
+
+            BluePrinter bluePrinter = new BluePrinter();
+            bluePrinter.Print("BluePrinter as BluePrinter");
+
+            GreenPrinter greenPrinter = new GreenPrinter();
+            greenPrinter.Print("GreenPrinter as GreenPrinter");
+
+            YellowPrinter yellowPrinter = new YellowPrinter();
+            yellowPrinter.Print("YellowPrinter as YellowPrinter");
+
+            basePrinter = bluePrinter;
+            basePrinter.Print("BluePrinter as Printer");
+
+            basePrinter = greenPrinter;
+            basePrinter.Print("GreenPrinter as Printer");
+
+            basePrinter = yellowPrinter;
+            basePrinter.Print("YellowPrinter as Printer");
         }
     }
 }
