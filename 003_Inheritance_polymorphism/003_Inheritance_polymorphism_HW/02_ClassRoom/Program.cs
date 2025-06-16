668225234bf4f847ec3c0f78b2653226f85a3255
cbb356 <cbb356@gmail.com>
2025-06-16 22:22:08 +0300

    003_04_HW added

--- 003_Inheritance_polymorphism/003_Inheritance_polymorphism_HW/02_ClassRoom/Program.cs
+++ 003_Inheritance_polymorphism/003_Inheritance_polymorphism_HW/02_ClassRoom/Program.cs
@@ -2,16 +2,6 @@
 {
     internal class Program
     {
-        /* Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
-         * Потрібно: Створити клас, який представляє навчальний клас ClassRoom. 
-         * Створіть клас учень Pupil. У тілі класу створіть методи void Study(), void Read(), 
-         * void Write(), void Relax(). Створіть 3 похідні класу ExcelentPupil, GoodPupil, BadPupil 
-         * від класу базового класу Pupil і перевизначте кожен із методів, залежно від успішності учня. 
-         * Конструктор класу ClassRoom приймає аргументи типу Pupil, клас має складатися із 4 учнів. 
-         * Передбачте можливість, що користувач може передати 2 або 3 аргументи. 
-         * Виведіть інформацію про те, як усі учні екземпляра класу ClassRoom 
-         * вміють вчитися, читати, писати, відпочивати. 
-         */
 
         static void Main(string[] args)
         {
@@ -38,6 +28,9 @@
 
             ClassRoom classRoom6 = new ClassRoom(excelentPupil1, excelentPupil2, goodPupil1, goodPupil2, badPupil);
             classRoom6.ShowPupilsInfo();
+
+            //Delay
+            Console.ReadKey();
         }
     }
 }
---
+++ 003_Inheritance_polymorphism/003_Inheritance_polymorphism_HW/04_DocumentWorker/DocumentWorker.cs
@@ -0,0 +1,41 @@
+﻿namespace Inheritance_polymorphism
+{
+    internal class DocumentWorker
+    {
+        public virtual void OpenDocument()
+        {
+            Console.WriteLine("The document was opened");
+        }
+
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("You can edit the document in the Pro version");
+        }
+
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("You can save the document in the Pro version");
+        }
+    }
+
+    internal class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("The document was edited");
+        }
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("You can save the document in the new format in the Expert version");
+        }
+    }
+
+    internal class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("The document was saved in the new format");
+        }
+    }
+}
