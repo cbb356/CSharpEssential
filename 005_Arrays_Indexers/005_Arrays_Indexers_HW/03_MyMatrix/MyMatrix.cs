91e7fdbe7bccab4c6aad4df4ebd69efdad07c595
cbb356 <cbb356@gmail.com>
2025-06-24 08:38:29 +0300

    005_03_HW partly added

---
+++ 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/MyMatrix.cs
@@ -0,0 +1,25 @@
+﻿namespace MyMatrix
+{
+    internal class MyMatrix
+    {
+        private int[,] matrix;
+
+        public MyMatrix(int rows, int colons)
+        {
+            _matrix = new int[rows, colons];
+            Random random = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(0, 100);
+                }
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            Console.WriteLine(""
+        }
+    }
+}
