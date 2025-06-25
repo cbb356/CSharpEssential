dbfe551f05e66fae896b23b54ca6bf8eaed603d0
cbb356 <cbb356@gmail.com>
2025-06-26 01:08:46 +0300

    005_03_HW added

--- 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/MyMatrix.cs
+++ 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/MyMatrix.cs
@@ -1,25 +1,93 @@
-﻿namespace MyMatrix
+﻿using System;
+
+namespace MyMatrix
 {
     internal class MyMatrix
     {
         private int[,] matrix;
 
-        public MyMatrix(int rows, int colons)
+        public int Rows => matrix.GetLength(0);
+        public int Columns => matrix.GetLength(1);
+
+
+        public MyMatrix(int rows, int columns)
         {
-            _matrix = new int[rows, colons];
+            matrix = new int[rows, columns];
             Random random = new Random();
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < columns; j++)
                 {
                     matrix[i, j] = random.Next(0, 100);
                 }
             }
         }
 
+        public int this[int row, int column]
+        {
+            get
+            {
+                return matrix[row, column];
+            }
+            set
+            {
+                matrix[row, column] = value;
+            }
+        }
+
         public void PrintMatrix()
         {
-            Console.WriteLine(""
+            PrintSubMatrix(Rows, Columns);
+        }
+
+        public void PrintSubMatrix(int rows, int columns)
+        {
+            if ((rows <= 0 || rows > Rows) || (columns <= 0 || columns > Columns))
+            {
+                Console.WriteLine("\nWrong values for rows or/and columns");
+                return;
+            }
+
+            Console.WriteLine("\nThe matrix with {0} rows and {1} columns:", rows, columns);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintMatrixRow(int row)
+        {
+            if (row >= Rows || row < 0)
+            {
+                Console.WriteLine("\nWrong value for the row");
+                return;
+            }
+
+            Console.WriteLine("\nThe {0} row of the matrix:", row);
+            for (int i = 0; i < Columns; i++)
+            {
+               Console.Write(matrix[row, i] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintMatrixColumn(int column)
+        {
+            if (column >= Columns || column < 0)
+            {
+                Console.WriteLine("\nWrong value for the column");
+                return;
+            }
+
+            Console.WriteLine("\nThe {0} column of the matrix:", column);
+            for (int i = 0; i < Rows; i++)
+            {
+                Console.Write(matrix[i, column] + "\n");
+            }
         }
     }
 }
--- 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/Program.cs
+++ 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/Program.cs
@@ -11,6 +11,30 @@
     {
         static void Main(string[] args)
         {
+            int rows, columns;
+            
+            Console.WriteLine("Enter the number of rows:");
+            while (!int.TryParse(Console.ReadLine(), out rows) || rows <= 0)
+                Console.WriteLine("You entered wrong value for number of rows. Try again");
+
+            Console.WriteLine("Enter the number of columns:");
+            while (!int.TryParse(Console.ReadLine(), out columns) || columns <= 0)
+                Console.WriteLine("You entered wrong value for number of columns. Try again");
+
+            MyMatrix myMatrix = new MyMatrix(rows, columns);
+            Random random = new Random();
+            
+            myMatrix.PrintMatrix();
+            myMatrix.PrintSubMatrix(random.Next(1, rows), random.Next(1, columns));
+            myMatrix.PrintSubMatrix(0, 0);
+            myMatrix.PrintSubMatrix((rows + 1), (columns + 1));
+            myMatrix.PrintMatrixRow(random.Next(0, rows));
+            myMatrix.PrintMatrixRow(-1);
+            myMatrix.PrintMatrixRow(rows);
+            myMatrix.PrintMatrixColumn(random.Next(0, columns));
+            myMatrix.PrintMatrixColumn(-1);
+            myMatrix.PrintMatrixColumn(columns);
+
             // Delay.
             Console.ReadKey();
         }
