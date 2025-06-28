167bbb124065daaa460d8a717f1e190ddf1380e7
cbb356 <cbb356@gmail.com>
2025-06-28 19:34:11 +0300

    005_03_HW changed. Some refactoring made

--- 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/MyMatrix.cs
+++ 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/MyMatrix.cs
@@ -4,16 +4,22 @@ namespace MyMatrix
 {
     internal class MyMatrix
     {
+        private Random random = new Random(); 
         private int[,] matrix;
 
-        public int Rows => matrix.GetLength(0);
-        public int Columns => matrix.GetLength(1);
+        public int Rows
+        {
+            get { return matrix.GetLength(0); }
+        }
+        public int Columns
+        {
+            get { return matrix.GetLength(1); }
+        }
 
 
         public MyMatrix(int rows, int columns)
         {
             matrix = new int[rows, columns];
-            Random random = new Random();
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
@@ -23,35 +29,12 @@ namespace MyMatrix
             }
         }
 
-        public int this[int row, int column]
-        {
-            get
-            {
-                return matrix[row, column];
-            }
-            set
-            {
-                matrix[row, column] = value;
-            }
-        }
-
         public void PrintMatrix()
         {
-            PrintSubMatrix(Rows, Columns);
-        }
-
-        public void PrintSubMatrix(int rows, int columns)
-        {
-            if ((rows <= 0 || rows > Rows) || (columns <= 0 || columns > Columns))
+            Console.WriteLine("The matrix with {0} rows and {1} columns:", Rows, Columns);
+            for (int i = 0; i < Rows; i++)
             {
-                Console.WriteLine("\nWrong values for rows or/and columns");
-                return;
-            }
-
-            Console.WriteLine("\nThe matrix with {0} rows and {1} columns:", rows, columns);
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < Columns; j++)
                 {
                     Console.Write(matrix[i, j] + "\t");
                 }
@@ -59,35 +42,29 @@ namespace MyMatrix
             }
         }
 
-        public void PrintMatrixRow(int row)
-        {
-            if (row >= Rows || row < 0)
+        public void ChangeMatrix(int newRows, int newColumns)
+        { 
+            int[,] newMatrix = new int[newRows, newColumns];
+
+            int oldRows = Rows;
+            int oldColumns= Columns;
+
+            for (int i = 0; i < newRows; i++)
             {
-                Console.WriteLine("\nWrong value for the row");
-                return;
+                for (int j = 0; j < newColumns; j++)
+                {
+                    if (i < oldRows && j < oldColumns)
+                    {
+                        newMatrix[i, j] = matrix[i, j];
+                    }
+                    else
+                    {
+                        newMatrix[i, j] = random.Next(0, 100);
+                    }
+                }
             }
 
-            Console.WriteLine("\nThe {0} row of the matrix:", row);
-            for (int i = 0; i < Columns; i++)
-            {
-               Console.Write(matrix[row, i] + "\t");
-            }
-            Console.WriteLine();
-        }
-
-        public void PrintMatrixColumn(int column)
-        {
-            if (column >= Columns || column < 0)
-            {
-                Console.WriteLine("\nWrong value for the column");
-                return;
-            }
-
-            Console.WriteLine("\nThe {0} column of the matrix:", column);
-            for (int i = 0; i < Rows; i++)
-            {
-                Console.Write(matrix[i, column] + "\n");
-            }
+            matrix = newMatrix;
         }
     }
 }
--- 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/Program.cs
+++ 005_Arrays_Indexers/005_Arrays_Indexers_HW/03_MyMatrix/Program.cs
@@ -1,39 +1,29 @@
 ﻿namespace MyMatrix
 {
     /*
-     * Створити клас MyMatrix, який забезпечує надання матриці довільного розміру з можливістю 
-     * зміни числа рядків і стовпців. Написати програму, яка виводить на екран матрицю і 
-     * похідні від неї матриці різних порядків.
-     */
-
+    * Створити клас MyMatrix, який забезпечує надання матриці довільного розміру з можливістю 
+    * зміни числа рядків і стовпців. Написати програму, яка виводить на екран матрицю і 
+    * похідні від неї матриці різних порядків.
+    */
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            int rows, columns;
-            
-            Console.WriteLine("Enter the number of rows:");
-            while (!int.TryParse(Console.ReadLine(), out rows) || rows <= 0)
-                Console.WriteLine("You entered wrong value for number of rows. Try again");
+            //Initial matrix
+            MyMatrix matrix = new MyMatrix(5, 5);
+            Console.WriteLine("Initial matrix");
+            matrix.PrintMatrix();
 
-            Console.WriteLine("Enter the number of columns:");
-            while (!int.TryParse(Console.ReadLine(), out columns) || columns <= 0)
-                Console.WriteLine("You entered wrong value for number of columns. Try again");
+            //Expanded matrix
+            matrix.ChangeMatrix(6, 7);
+            Console.WriteLine("\nExpanded matrix");
+            matrix.PrintMatrix();
 
-            MyMatrix myMatrix = new MyMatrix(rows, columns);
-            Random random = new Random();
-            
-            myMatrix.PrintMatrix();
-            myMatrix.PrintSubMatrix(random.Next(1, rows), random.Next(1, columns));
-            myMatrix.PrintSubMatrix(0, 0);
-            myMatrix.PrintSubMatrix((rows + 1), (columns + 1));
-            myMatrix.PrintMatrixRow(random.Next(0, rows));
-            myMatrix.PrintMatrixRow(-1);
-            myMatrix.PrintMatrixRow(rows);
-            myMatrix.PrintMatrixColumn(random.Next(0, columns));
-            myMatrix.PrintMatrixColumn(-1);
-            myMatrix.PrintMatrixColumn(columns);
+            //Contracted matrix
+            matrix.ChangeMatrix(4, 3);
+            Console.WriteLine("\nContracted matrix");
+            matrix.PrintMatrix();
 
             // Delay.
             Console.ReadKey();
