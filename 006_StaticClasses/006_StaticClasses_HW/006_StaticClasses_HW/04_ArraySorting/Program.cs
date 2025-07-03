aada376644d46b1abcb1433068932bb9910d2185
cbb356 <cbb356@gmail.com>
2025-07-03 23:16:01 +0300

    006_04_HW added

--- 006_StaticClasses/006_StaticClasses_HW/006_StaticClasses_HW/04_ArraySorting/Program.cs
+++ 006_StaticClasses/006_StaticClasses_HW/006_StaticClasses_HW/04_ArraySorting/Program.cs
@@ -4,12 +4,46 @@
      * Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
      * Потрібно: створити розширюючий метод для цілого масиву, який сортує елементи масиву за зростанням. 
      */
+    internal static class ExtensionClass
+    {
+        public static void SortArray(this int[] array)
+        {
+            Array.Sort(array);
+        }
+    }
+
 
     internal class Program
     {
+        static void ShowArray(string name, int[] array)
+        {
+            Console.Write(name);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < (array.Length - 1))
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 100);
+            }
+
+            ShowArray("Initial array:", array);
+            array.SortArray();
+            ShowArray("Sorted array:", array);
 
             // Delay.
             Console.ReadKey();
