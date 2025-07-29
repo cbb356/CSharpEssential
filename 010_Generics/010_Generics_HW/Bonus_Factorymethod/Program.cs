7c3473d2fe4c9aefbae5c787f52f454fbac87977
cbb356 <cbb356@gmail.com>
2025-07-29 10:18:50 +0300

    010_HW templates added

---
+++ 010_Generics/010_Generics_HW/Bonus_Factorymethod/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+ * Створіть  клас  MyClass  <T>,  що  містить  статичний  фабричний  метод  -  T  FacrotyMethod  (),  
+ * який  буде породжувати екземпляри типу, зазначеного в якості параметра типу 
+ * (покажчика місця заповнення типом - Т). 
+ */
+
+namespace Bonus_Factorymethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
