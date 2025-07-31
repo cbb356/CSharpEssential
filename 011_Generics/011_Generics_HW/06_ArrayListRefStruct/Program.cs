de2106ff5f1b4501ed12e5fdcca7920c2eeef889
cbb356 <cbb356@gmail.com>
2025-07-31 23:56:29 +0300

    011_HW created

---
+++ 011_Generics/011_Generics_HW/06_ArrayListRefStruct/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArrayListRefStruct
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
