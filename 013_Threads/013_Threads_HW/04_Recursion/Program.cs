390d608de76b9f8809a45f528a3ebb5c749bff41
cbb356 <cbb356@gmail.com>
2025-08-12 02:50:07 +0300

    013_04_HW added

---
+++ 013_Threads/013_Threads_HW/04_Recursion/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+ * Напишіть програму, в якій метод викликатиметься рекурсивно. 
+ * Кожен новий виклик методу виконується окремому потоці.
+ */
+
+using System.Diagnostics.Metrics;
+
+namespace Recursion
+{
+
+    internal class Program
+    {
+        private static readonly int MaxDepth = 5;
+        static void Recursive(int depth)
+        {
+            int hash = Thread.CurrentThread.GetHashCode();
+
+            Console.WriteLine($"Entering {depth} on Thread {hash}");
+            if (depth > 1)
+            {
+                Thread newThread = new Thread(() => Recursive(depth - 1));
+                newThread.Start();
+                newThread.Join();
+            }
+            Console.WriteLine($"Exiting {depth} on Thread {hash}");
+        }
+        static void Main(string[] args)
+        {
+            Recursive(MaxDepth);
+
+            // Delay
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
